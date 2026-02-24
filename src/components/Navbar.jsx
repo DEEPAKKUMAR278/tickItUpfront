31dea8ca5c8509d4695ff6ea332c98cc98d6be1d
himusinggit <hbilluscoding@gmail.com>
2026-02-24 10:13:12 +0530

    basic template ready

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <>
+            <h1 className='bg-red-500'>heloo deepak bhaoi</h1>
+        </>
+    )
+}
+
+export default Navbar
